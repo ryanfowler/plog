a119f46b3ef916006826394fa1836e22a11cc55e
Ryan Fowler <ryan.fowler19@gmail.com>
2021-07-24 22:49:37 +0000

    Handle io errors correctly

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,16 @@ mod logfmt;
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn main() -> io::Result<()> {
     let formats = vec![json::JsonFormat::new(), logfmt::LogFormat::new()];
     let mut handler = log::Handler::new(formats);
 
     let mut stdout = BufWriter::new(io::stdout());
     let mut lines = BufReader::new(io::stdin()).lines();
-    while let Some(line) = lines.next_line().await.unwrap() {
+    while let Some(line) = lines.next_line().await? {
         let out = handler.handle(&line);
-        stdout.write(out.as_bytes()).await.unwrap();
-        stdout.flush().await.unwrap();
+        stdout.write(out.as_bytes()).await?;
+        stdout.flush().await?;
     }
+    Ok(())
 }
